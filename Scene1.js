a5cf8c4a3c6e0227811b425918bf356eab8e9b33
Aryansh <89254856+4127157@users.noreply.github.com>
2022-07-20 15:18:00 +0530

    add moving ships and bg

--- Scene1.js
+++ Scene1.js
@@ -4,6 +4,15 @@ class Scene1 extends Phaser.Scene {
         super("bootGame");
     }
 
+    preload()
+    {
+        this.load.image("background", "./assets/space_shooter_pack/Graphics/backgrounds/desert-background.png");
+
+        this.load.image("ship1", "./assets/space_shooter_pack/Graphics/spritesheets/enemy-small.png");
+        this.load.image("ship2", "./assets/space_shooter_pack/Graphics/spritesheets/enemy-medium.png");
+        this.load.image("ship3", "./assets/space_shooter_pack/Graphics/spritesheets/enemy-big.png");
+    }
+
     create()
     {
         this.add.text(20,20, "Loading game....");

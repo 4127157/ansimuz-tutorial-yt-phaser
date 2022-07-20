4f139a2e6d00a1b7e75a751ffc0f36e84497351e
Aryansh <89254856+4127157@users.noreply.github.com>
2022-07-21 04:31:54 +0530

    add movement to ship

--- Scene1.js
+++ Scene1.js
@@ -30,12 +30,85 @@ class Scene1 extends Phaser.Scene {
             frameWidth: 16,
             frameHeight: 16
         });
+
+        this.load.spritesheet("player", "./assets/space_shooter_pack/Graphics/spritesheets/ship.png", {
+            frameWidth: 16,
+            frameHeight: 24
+        });
+
+        this.load.spritesheet("bullet", "./assets/space_shooter_pack/Graphics/spritesheets/laser-bolts.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
     }
 
     create()
     {
         this.add.text(20,20, "Loading game....");
         this.scene.start("playGame");
+
+        this.anims.create({
+            key: "ship1_anim",
+            frames: this.anims.generateFrameNumbers("ship1"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "ship2_anim",
+            frames: this.anims.generateFrameNumbers("ship2"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "ship3_anim",
+            frames: this.anims.generateFrameNumbers("ship3"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "explo_anim",
+            frames: this.anims.generateFrameNumbers("explosion"),
+            frameRate: 20,
+            repeat: 0,
+            hideOnComplete: true
+        });
+
+        this.anims.create({
+            key: "red",
+            frames: this.anims.generateFrameNumbers("power-up", {
+                start:0,
+                end: 1
+            }),
+            frameRate: 5,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "gray",
+            frames: this.anims.generateFrameNumbers("power-up", {
+                start:2,
+                end: 3
+            }),
+            frameRate: 5,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "thrust",
+            frames: this.anims.generateFrameNumbers("player"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "fire_anim",
+            frames: this.anims.generateFrameNumbers("bullet"),
+            frameRate: 20,
+            repeat: -1
+        });
     }
 }
 

5168f432f0487479ff14a85e31dff5c9671bb519
Aryansh <89254856+4127157@users.noreply.github.com>
2022-07-20 15:41:55 +0530

    make ships destructible on click

--- Scene1.js
+++ Scene1.js
@@ -8,9 +8,24 @@ class Scene1 extends Phaser.Scene {
     {
         this.load.image("background", "./assets/space_shooter_pack/Graphics/backgrounds/desert-background.png");
 
-        this.load.image("ship1", "./assets/space_shooter_pack/Graphics/spritesheets/enemy-small.png");
-        this.load.image("ship2", "./assets/space_shooter_pack/Graphics/spritesheets/enemy-medium.png");
-        this.load.image("ship3", "./assets/space_shooter_pack/Graphics/spritesheets/enemy-big.png");
+        this.load.spritesheet("ship1", "./assets/space_shooter_pack/Graphics/spritesheets/enemy-small.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+        this.load.spritesheet("ship2", "./assets/space_shooter_pack/Graphics/spritesheets/enemy-medium.png", {
+            frameWidth: 32,
+            frameHeight: 16
+        });
+        this.load.spritesheet("ship3", "./assets/space_shooter_pack/Graphics/spritesheets/enemy-big.png", {
+            frameWidth: 32,
+            frameHeight: 32
+        });
+
+        this.load.spritesheet("explosion", "./assets/space_shooter_pack/Graphics/spritesheets/explosion.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+
     }
 
     create()

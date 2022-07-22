f624f45836b448e928d55dfcb3e7b2cf5eda7ab9
Aryansh <89254856+4127157@users.noreply.github.com>
2022-07-23 05:07:44 +0530

    make powerups collectible and kill player with enemy collsn

--- Scene2.js
+++ Scene2.js
@@ -12,6 +12,11 @@ class Scene2 extends Phaser.Scene {
         this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2");
         this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
 
+        this.enemies = this.physics.add.group();
+        this.enemies.add(this.ship1);
+        this.enemies.add(this.ship2);
+        this.enemies.add(this.ship3);
+
         this.ship1.play("ship1_anim");
         this.ship2.play("ship2_anim");
         this.ship3.play("ship3_anim");
@@ -57,9 +62,28 @@ class Scene2 extends Phaser.Scene {
 
         this.projectiles = this.add.group();
 
-        this.physics.add.collider(this.projectiles, this.powerUps);
+        this.physics.add.collider(this.projectiles, 
+            this.powerUps,
+            function(projectile, powerUp) {
+                projectile.destroy();
+            }
+        );
 
+        this.physics.add.overlap(this.player, this.powerUps, this.pickPowerUp, null, this);
+        
+        this.physics.add.overlap(this.player, this.enemies, this.hurtPlayer, null, this);
     }
+
+    pickPowerUp(player, powerUp) {
+        powerUp.disableBody(true, true);
+    }
+
+    hurtPlayer(player, enemy){
+        //this.resetShipPos(enemy);
+        player.x = config.width / 2 - 8;
+        player.y = config.height - 64;
+    }
+
 
     update(){
         this.moveShip(this.ship1, 1);
